c00da03df88b704ed7afcb68dfd25197d925526d
Xori <xori93@aol.com>
2025-07-13 20:24:23 -0400

    lvl1-w3-o8-arrays

--- JS-Fundamentals/Project-08-Arrays/mentee/script.js
+++ JS-Fundamentals/Project-08-Arrays/mentee/script.js
@@ -26,6 +26,21 @@ const totalCount = document.getElementById("totalCount");
 //       - append it to the <ul>
 // 3.4 – Update the totalCount paragraph with guestList.length
 
+function renderList() {
+  guestList.innerHTML = "";
+  
+  for (i = 1; i <= guestList.lenght; i++) {
+    const guestName = guestList[i];
+
+    const li = document.createElement("li");
+    li.innerText = guestName;
+    guestList.appendChild(li)
+
+    totalCount.innerText = `Total guests: ${guestList.lenght}`; 
+  }
+}
+
+
 // ---------- STEP 4: Create a function called handleAdd() ----------
 // Inside the function:
 // 4.1 – Get the value from the input box and trim it
@@ -34,15 +49,49 @@ const totalCount = document.getElementById("totalCount");
 // 4.4 – Clear the input field
 // 4.5 – Call renderList() to update the UI
 
+function handleAdd() {
+  const name = guestInput.value.trim()
+
+  if (name === "") {
+    alert("Please enter guest name");
+    return;
+  }
+
+  guestList.push(name);
+
+  guestInput.value = "";
+
+  renderList();
+}
+
+
 // ---------- STEP 5: Create a function called handleRemoveLast() ----------
 // Inside the function:
 // 5.1 – If the array is empty, alert the user and stop
 // 5.2 – Otherwise, use pop() to remove the last name
 // 5.3 – Call renderList() to update the UI
 
+function handleRemoveLast() {
+  if (guestList.length === 0) {
+    alert("list empty")
+    return;
+  }
+
+  guestList.pop();
+
+  renderList();
+}
+
 // ---------- STEP 6: Add event listeners ----------
 // Add a click event to the Add button that calls handleAdd()
 // Add a click event to the Remove Last button that calls handleRemoveLast()
 
+addBtn.addEventListener("click", handleAdd)
+
+removeBtn.addEventListener("click", handleRemoveLast)
+
+
 // ---------- STEP 7: Call renderList() at the end ----------
 // This makes sure the list is empty and count is 0 when the page loads
+
+renderList()
